df713599932de349cac3f1656438a119c1898428
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-07-15 16:41:13 -0300

    format json

--- recursos/variables.py
+++ recursos/variables.py
@@ -4,139 +4,142 @@ BLACK = 'BLACK'
 
 GOLD = 'GOLD'
 
-ENTRADA = [{
-"numero": 100001,
-"nombre": "Nicolas",
-"apellido": "Gaston",
-"dni": "29494777",
-"tipo": "BLACK",
-"direccion" :{
-"calle" : "Rivadavia",
-"numero" : "7900",
-"ciudad": "Capital Federal",
-"provincia" : "Buenos Aires",
-"pais": "Argentina"
-},
-"transacciones": [{
-"estado": "ACEPTADA",
-"tipo": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 9000,
-"cantidadExtraccionesHechas": 1,
-"monto": 1000,
-"fecha": "06/06/2022 10:00:55",
-"numero": 1,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "RECHAZADA",
-"tipo": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 10000,
-"monto": 11000,
-"fecha": "06/06/2022 10:10:55",
-"numero": 2,
-"saldoEnCuenta": 10000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "RECHAZADA",
-"tipo": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 9000,
-"fecha": "06/06/2022 10:22:55",
-"numero": 3,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "RECHAZADA",
-"tipo": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 9000,
-"fecha": "06/06/2022 10:33:33",
-"numero": 4,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "RECHAZADA",
-"tipo": "ALTA_TARJETA_CREDITO",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 9000,
-"fecha": "06/06/2022 12:00:00",
-"numero": 5,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "RECHAZADA",
-"tipo": "ALTA_CHEQUERA",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 9000,
-"fecha": "06/06/2022 12:30:55",
-"numero": 6,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "RECHAZADA",
-"tipo": "COMPRA_DOLAR",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 9000,
-"fecha": "06/06/2022 12:45:33",
-"numero": 7,
-"saldoEnCuenta": 5000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "RECHAZADA",
-"tipo": "TRANSFERENCIA_ENVIADA",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 1000000,
-"fecha": "06/06/2022 13:00:55",
-"numero": 8,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "ACEPTADA",
-"tipo": "TRANSFERENCIA_RECIBIDA",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 9000,
-"fecha": "06/06/2022 13:11:15",
-"numero": 9,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-},
-{
-"estado": "ACEPTADA",
-"tipo": "TRANSFERENCIA_RECIBIDA",
-"cuentaNumero": 190,
-"cupoDiarioRestante": 3000,
-"monto": 200000,
-"fecha": "06/06/2022 16:00:55",
-"numero": 10,
-"saldoEnCuenta": 100000,
-"totalTarjetasDeCreditoActualmente" : 5,
-"totalChequerasActualmente" : 2
-}
+ENTRADA = [
+    {
+        "numeroCliente": 100001,
+        "nombre": "Nicolas",
+        "apellido": "Gaston",
+        "dni": "29494777",
+        "tipoCliente": "BLACK",
+        "direccion": {
+            "calle": "Rivadavia",
+            "numeroCalle": "7900",
+            "ciudad": "Capital Federal",
+            "provincia": "Buenos Aires",
+            "pais": "Argentina"
+        },
+        "transacciones": [
+            {
+                "estado": "ACEPTADA",
+                "tipoTransaccion": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 9000,
+                "cantidadExtraccionesHechas": 1,
+                "montoTransaccion": 1000,
+                "fecha": "06/06/2022 10:00:55",
+                "numeroTransaccion": 1,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "RECHAZADA",
+                "tipoTransaccion": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 10000,
+                "montoTransaccion": 11000,
+                "fecha": "06/06/2022 10:10:55",
+                "numeroTransaccion": 2,
+                "saldoEnCuenta": 10000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "RECHAZADA",
+                "tipoTransaccion": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 9000,
+                "fecha": "06/06/2022 10:22:55",
+                "numeroTransaccion": 3,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "RECHAZADA",
+                "tipoTransaccion": "RETIRO_EFECTIVO_CAJERO_AUTOMATICO",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 9000,
+                "fecha": "06/06/2022 10:33:33",
+                "numeroTransaccion": 4,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "RECHAZADA",
+                "tipoTransaccion": "ALTA_TARJETA_CREDITO",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 9000,
+                "fecha": "06/06/2022 12:00:00",
+                "numeroTransaccion": 5,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "RECHAZADA",
+                "tipoTransaccion": "ALTA_CHEQUERA",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 9000,
+                "fecha": "06/06/2022 12:30:55",
+                "numeroTransaccion": 6,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "RECHAZADA",
+                "tipoTransaccion": "COMPRA_DOLAR",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 9000,
+                "fecha": "06/06/2022 12:45:33",
+                "numeroTransaccion": 7,
+                "saldoEnCuenta": 5000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "RECHAZADA",
+                "tipoTransaccion": "TRANSFERENCIA_ENVIADA",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 1000000,
+                "fecha": "06/06/2022 13:00:55",
+                "numeroTransaccion": 8,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "ACEPTADA",
+                "tipoTransaccion": "TRANSFERENCIA_RECIBIDA",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 9000,
+                "fecha": "06/06/2022 13:11:15",
+                "numeroTransaccion": 9,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            },
+            {
+                "estado": "ACEPTADA",
+                "tipoTransaccion": "TRANSFERENCIA_RECIBIDA",
+                "cuentaNumero": 190,
+                "cupoDiarioRestante": 3000,
+                "montoTransaccion": 200000,
+                "fecha": "06/06/2022 16:00:55",
+                "numeroTransaccion": 10,
+                "saldoEnCuenta": 100000,
+                "totalTarjetasDeCreditoActualmente": 5,
+                "totalChequerasActualmente": 2
+            }
+        ]
+    }
 ]
-}]
