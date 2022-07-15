8207f7691e152b96ecb332e257a624ec715654be
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-07-15 20:25:56 -0300

    Se cambia el metodo especial de clase

--- recursos/class_errores.py
+++ recursos/class_errores.py
@@ -1,7 +1,7 @@
 # Segun el .pdf, los motivos por los cuales la transferencia
 # fallo deben estar listados como clases(Exception)
 
-class MontoNegativo(Exception):
+class MontoNegativo():
     def __init__(self, monto, saldo, message="Saldo insuficiente") -> None:
         "Monto y Saldo seran utilizados al invocar el error"
         self.monto = monto
@@ -9,5 +9,5 @@ class MontoNegativo(Exception):
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self) -> str:
-        return f"${self.saldo}-${self.monto} ->{self.message}"
+    def __repr__(self) -> str:
+        return f'<{self.message} se pretende gastar {self.monto} con un saldo de {self.saldo}>'
